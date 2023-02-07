4bff15b2d3976d31e2d26efee69141a50c3dc36e
cristi <scarlat.cristian.gabriel@gmail.com>
2023-02-07 19:59:23 +0200

    finished the main function and design

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,22 +1,129 @@
 const socket = io();
 
 // Elements
-const form = document.querySelector("#message-form");
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
 
-socket.on("countUpdated", (count) => {
-  console.log("The count has been updated!", count);
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector(
+  "#location-message-template"
+).innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
+
+// Options
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
 });
 
-socket.on("helloMessage", (message) => {
-  console.log(message);
-});
+const autoscroll = () => {
+  // New message element - we grab the new message
+  const $newMessage = $messages.lastElementChild;
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const message = e.target.elements.message.value;
-  socket.emit("sendMessage", message);
+  // Height of the new message
+  // We get the style of the new message
+  const newMessageStyles = getComputedStyle($newMessage);
+  // We get the margin from the style
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  // We add the margin to the message height
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
 
-  socket.on("message", (message) => {
-    console.log(message);
+  // Visible height
+  const visibleHeight = $messages.offsetHeight;
+
+  // Height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  // How far have i scrolled
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
+
+socket.on("message", (message) => {
+  const html = Mustache.render(messageTemplate, {
+    username: message.username,
+    message: message.text,
+    createdAt: moment(message.createdAt).format("k:mm"),
   });
+  $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
+});
+
+socket.on("locationMessage", (message) => {
+  console.log(message);
+  const html = Mustache.render(locationMessageTemplate, {
+    username: message.username,
+    url: message.url,
+    createdAt: moment(message.createdAt).format("k:mm"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
+});
+
+socket.on("roomData", ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room: room,
+    users: users,
+  });
+  document.querySelector("#sidebar").innerHTML = html;
+});
+
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //   disable form
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  //   inputMessage is the name of the input field in the HTML form from which we take the value
+  const message = e.target.elements.inputMessage.value;
+  socket.emit("sendMessage", message, (error) => {
+    // enable form
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log("Message delivered!");
+  });
+});
+
+$sendLocationButton.addEventListener("click", () => {
+  // checking if users have support for geolocation
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+
+  // disable button
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      (acknowlegment) => {
+        // enable button
+        $sendLocationButton.removeAttribute("disabled");
+        console.log(acknowlegment);
+      }
+    );
+  });
+});
+
+socket.emit("join", { username, room }, (error) => {
+  if (error) {
+    alert(error);
+    location.href = "/";
+  }
 });
